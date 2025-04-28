4b423d3a43c189ef65dada5240725ea772b7e7ce
ArielS <diciembre93@gmail.com>
2025-04-28 12:49:29 -0300

    pequeñas correciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,10 @@ import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/layout/header/header.component";
 import { FooterComponent } from "./components/layout/footer/footer.component";
-import { Punto1Component } from "./components/punto1/punto1.component";
-import { Punto2Component } from "./components/punto2/punto2.component";
-import { Punto3Component } from "./components/punto3/punto3.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, Punto1Component, Punto2Component, Punto3Component],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
